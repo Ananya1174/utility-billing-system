613f7b550be80616c759940eb6b5fbad4c5df610
Ananya1174 <ananyaarisenapalli@gmail.com>
2026-01-02 10:44:24 +0530

    added home page

---
+++ frontend/utility-billing-system/src/app/features/pages/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../../shared/navbar/navbar';
+@Component({
+  selector: 'app-home',
+  imports: [Navbar],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
